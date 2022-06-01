e32b9d6ef316a585dcce7c96a70a8a8b600c1375
Xaver Pinero <xaverpinero@yahoo.com>
2022-06-01 07:18:19 -0400

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import styled from "styled-components";
+import landing from '../images/landing.jpg';
 
-const landing = require('../images/landing.jpg');
+const LandingImage = styled.div`
+	background-image: url(${landing})
+	height: 700px;
+	`
 
 const Home = () => {
 return (
-	<div>
-	<imageBackground source={landing}>
+	<LandingImage>
 		<h1>Welcome to my Real-estate page!</h1>
-	</imageBackground>
-	
-	</div>
+	</LandingImage>
 );
 };
 
