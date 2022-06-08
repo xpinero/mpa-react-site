4dd2297fa0624631866160f6236998b9df5a19bb
Xaver Pinero <xaverpinero@yahoo.com>
2022-06-07 20:30:40 -0400

    added styled components to iframe for resizing issue

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,12 +1,22 @@
 import React from "react";
+import styled from 'styled-components';
+
+const SearchFrame = styled.div`
+	height: 80vh;
+	width: 100vw;
+	display: block;
+	margin-left: 10%;
+	margin-right: auto;
+	margin-top: 1%;
+`
 
 const Search = () => {
 return (
-	<div>
-	<h1>
-  <iframe id="advanced_iframe" name="iframe1" src="https://homeasap.com/chadtrapp" width="1420" height="715" frameborder="0" border="0" allowtransparency="true" title="chadsFrame"></iframe>
-	</h1>
-	</div>
+	<SearchFrame>
+	
+  <iframe id="advanced_iframe" name="iframe1" src="https://homeasap.com/chadtrapp" width="80%" height="715" frameborder="0" border="0" allowtransparency="true" title="chadsFrame"></iframe>
+	
+	</SearchFrame>
 );
 };
 
